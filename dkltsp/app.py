911345d39162f004efb062ce0099086bc6c57a14
Daewon Kwon <sliwet@gmail.com>
2019-12-15 10:05:03 -0500

    Commit before fixing

--- dkltsp/app.py
+++ dkltsp/app.py
@@ -1,5 +1,5 @@
 import os
-import dkltsp.ltsp
+import ltsp
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -11,8 +11,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# ENV = 'dev'
-ENV = 'prod'
+ENV = 'dev'
+# ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
