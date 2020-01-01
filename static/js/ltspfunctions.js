110706cabd11beefc05b8a2a38c511ca0b2239d7
Daewon Kwon <sliwet@gmail.com>
2020-01-01 07:58:55 -0500

    Remove polyfill

--- static/js/ltspfunctions.js
+++ static/js/ltspfunctions.js
@@ -1,3 +1,28 @@
+let lastTime = 0;
+let vendors = ['ms', 'moz', 'webkit', 'o'];
+for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+        || window[vendors[x] + 'CancelRequestAnimationFrame'];
+}
+
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (callback, element) => {
+        let currTime = new Date().getTime();
+        let timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        let id = window.setTimeout(() => { callback(currTime + timeToCall); },
+            timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+    };
+}
+
+if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = id => {
+        clearTimeout(id);
+    };
+}
+
 let wait = ms => {
     let d = new Date();
     let d2 = null;
